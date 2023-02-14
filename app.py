4e2b56cc38c07fa833672afeb437095620d9576f
ki3ani <kimanim68@gmailcom>
2023-02-14 19:46:38 +0300

    auth-top-tracks

--- app.py
+++ app.py
@@ -16,23 +16,47 @@ app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 
 
+
 @app.route('/auth')
 
 def auth():
     
-        scope = 'user-read-private user-read-email'
     
-        sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id=os.getenv('SPOTIPY_CLIENT_ID'),
+        sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id=os.getenv('CLIENT_ID'),
     
-        client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'),
+        client_secret=os.getenv('CLIENT_SECRET'),
     
-        redirect_uri=os.getenv('SPOTIPY_REDIRECT_URI'),
+        redirect_uri='http://localhost:5000/callback',
     
-        scope=scope)
+        scope='user-top-read')
     
         auth_url = sp_oauth.get_authorize_url()
     
         return redirect(auth_url)
+
+
+@app.route('/callback')
+def callback():
+    sp_oauth = SpotifyOAuth(client_id=os.getenv('CLIENT_ID'),
+                            client_secret=os.getenv('CLIENT_SECRET'),
+                            redirect_uri='http://localhost:5000/callback',
+                            scope='user-top-read')
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    access_token = token_info['access_token']
+    sp = spotipy.Spotify(auth=access_token)
+    top_tracks = sp.current_user_top_tracks(time_range='short_term', limit=10)
+    track_list = []
+    for track in top_tracks['items']:
+        track_name = track['name']
+        artist_name = track['artists'][0]['name']
+        track_list.append(f"{track_name} by {artist_name}")
+    return render_template('top_tracks.html', tracks=track_list)
+
+
+
+
+
 
 
 
